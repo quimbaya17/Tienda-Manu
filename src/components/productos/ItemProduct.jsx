915322968ddc9cbd3e08065903bc49b8c985e142
GuillermoSamboni <guillermosamboni0912@gmail.com>
2023-10-23 21:52:27 -0500

    resolver error de listado para exsoneracion de examen

---
+++ src/components/productos/ItemProduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ItemProducto = ({ id, title, description, images }) => {
+  return (
+    <div className="mt-10 bg-white p-4  flex-1 flex-co rounded-lg shadow-lg">
+      <img src={images} alt="product" className="h-12 gap-0" />
+      <h1 className="text-center text-blue-500 font-bold">{title}</h1>
+      <p className="text-start font-medium mt-6">{description}</p>
+      <button className="bg-blue-600 shadow-lg text-white rounded-lg p-2 mt-6">
+        Ver detalle
+      </button>
+    </div>
+  );
+};
+
+export default ItemProducto;
--- src/views/Productos/ProductsView.jsx
+++ src/views/Productos/ProductsView.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LayoutComponent from "../../components/Layout/LayoutComponent";
-import { Link } from "react-router-dom";
+import ItemProducto from "../../components/productos/ItemProduct";
 
 function ProductsView() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <LayoutComponent>
       <div>
         <div className="flex w-full">
           <h1>Listado de productos</h1>
-          <Link
-            to="/CreateProduct"
-            className="justify-end float-right bottom-2 bg-green-600 rounded-lg p-2 text-white border-l"
-          >
-            Crear nuevo producto
-          </Link>
+          <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+            {products?.map((product) => {
+              <ItemProducto
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                image={product.images[0]}
+              />;
+            })}
+          </div>
         </div>
       </div>
     </LayoutComponent>
